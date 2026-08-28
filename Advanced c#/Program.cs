50afba9630d5ab886adbe9a3285ba8de3f1c7a95
shr0ukgomaa33 <shroukgomaa8787@gmail.com>
2026-08-28 19:06:08 +0300

    Q1

--- Advanced c#/Program.cs
+++ Advanced c#/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Advanced_c_
+﻿using System.Runtime.Intrinsics.X86;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Advanced_c_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            #region Q1 
+
+            // : What is a generic class? Why use generics?**
+            // generic class is a class that uses a type parameter, allowing it to work with different data types.
+
+
+            //We use generics because they: 
+            //Provide type safety.
+          //  Improve code reusability.
+//             Reduce the need for casting.
+                //Make code more flexible and maintainable.
+
+
+
+            #endregion
+
+
+
+
         }
     }
 }
